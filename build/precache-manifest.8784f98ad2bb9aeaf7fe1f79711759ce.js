63d05701c93e9f1f4ae9e3bb1d65e9ce55376b9c
sanved77 <sanved77@gmail.com>
2020-08-21 17:38:42 -0700

    Build added

---
+++ build/precache-manifest.8784f98ad2bb9aeaf7fe1f79711759ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0edb9a24ba18fd61730cbdb4acb93b5",
+    "url": "/anagram/index.html"
+  },
+  {
+    "revision": "8dfff55c94db99bad6e4",
+    "url": "/anagram/static/css/main.43136d3b.chunk.css"
+  },
+  {
+    "revision": "dcfdf08e63db5fd94cda",
+    "url": "/anagram/static/js/2.acb07290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anagram/static/js/2.acb07290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dfff55c94db99bad6e4",
+    "url": "/anagram/static/js/main.672c9f1f.chunk.js"
+  },
+  {
+    "revision": "247102a8c6718dbeca8b",
+    "url": "/anagram/static/js/runtime-main.c721f8a9.js"
+  }
+]);

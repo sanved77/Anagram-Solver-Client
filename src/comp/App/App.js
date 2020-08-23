6b2b15baaa440a2b734eb66b4abc48046fd7a681
sanved77 <sanved77@gmail.com>
2020-08-22 20:08:54 -0700

    Functional components

--- src/comp/App/App.js
+++ src/comp/App/App.js
@@ -1,37 +1,29 @@
 import React, { Component } from "react";
 import Panel from "../Panel/Panel";
 import "./App.scss";
+import PlaceHolder from "../PlaceHolder/PlaceHolder";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searched: false,
-      term: "",
-      data: {},
-      loading: "",
-      speak: false,
-      warning: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.grabTheAnagrams = this.grabTheAnagrams.bind(this);
-    this.keyPress = this.keyPress.bind(this);
-  }
+function App() {
+  let searched = false;
+  let term = "";
+  let data = {};
+  let loading = "";
+  let speak = false;
+  let warning = false;
 
-  handleChange(e) {
-    this.setState({
-      term: e.target.value,
-    });
-  }
+  const handleChange = (e) => {
+    term = e.target.value;
+  };
 
-  keyPress(e) {
+  const keyPress = (e) => {
     if (e.keyCode === 13) {
       this.grabTheAnagrams();
     }
-  }
+  };
 
-  grabTheAnagrams() {
-    if (/^[A-Za-z]+$/.test(this.state.term)) {
+  const grabTheAnagrams = () => {
+    term = term.replace(/\s+/g, "");
+    if (/^[A-Za-z]+$/.test(term)) {
       this.setState({
         loading: "is-loading",
         warning: false,
@@ -46,65 +38,49 @@ class App extends Component {
       })
         .then((response) => response.json())
         .then((wdata) => {
-          this.setState({
-            data: wdata,
-            searched: true,
-          });
+          data = wdata;
+          searched = true;
         })
         .catch((e) => {
           console.log(e);
         })
         .finally(() => {
-          this.setState({
-            loading: "",
-          });
+          loading = "";
         });
     } else {
-      this.setState({
-        warning: true,
-      });
+      warning = true;
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <div className="head">
-          <span>Anagram Solver</span>
+  return (
+    <div className="App">
+      <div className="head">
+        <span>Anagram Solver</span>
+      </div>
+      <div className="search">
+        <div className="search-box">
+          <input maxlength="20" onKeyDown={keyPress} value={term} className="input is-info is-large" onChange={handleChange} type="text" placeholder="enter a word" />
         </div>
-        <div className="search">
-          <div className="search-box">
-            <input onKeyDown={this.keyPress} value={this.state.term} className="input is-info is-large" onChange={this.handleChange} type="text" placeholder="enter a word" />
-          </div>
-          <div className="search-btn">
-            <button disabled={!this.state.term} onClick={this.grabTheAnagrams} className={this.state.loading + " button is-info is-large"}>
-              <span className="icon is-medium">
-                <i className="fas fa-search"></i>
-              </span>
-              <span>Search</span>
-            </button>
-          </div>
-        </div>
-        {this.state.warning && <div className="warning">Please only enter alphabets</div>}
-        {!this.state.searched && (
-          <div className="placeholder">
-            <p className="place-title">Anagrams</p>
-            <p className="phonetic">/ˈanəˌɡram/</p>
-            <p className="noun">noun</p>
-            <p className="meaning">“a word or phrase made by using the letters of another word or phrase in a different order”</p>
-            <p className="example">“Angel” is an anagram of “glean.”</p>
-          </div>
-        )}
-        <div className="panels">
-          {Object.keys(this.state.data)
-            .reverse()
-            .map((key, i) => (
-              <Panel key={i} title={key} data={this.state.data[key]}></Panel>
-            ))}
+        <div className="search-btn">
+          <button disabled={!term} onClick={grabTheAnagrams} className={loading + " button is-info is-large"}>
+            <span className="icon is-medium">
+              <i className="fas fa-search"></i>
+            </span>
+            <span>Search</span>
+          </button>
         </div>
       </div>
-    );
-  }
+      {warning && <div className="warning">Please only enter alphabets</div>}
+      {!searched && <PlaceHolder></PlaceHolder>}
+      <div className="panels">
+        {Object.keys(data)
+          .reverse()
+          .map((key, i) => (
+            <Panel key={i} title={key} data={data[key]}></Panel>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/comp/PlaceHolder/PlaceHolder.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./PlaceHolder.scss";
+
+const PlaceHolder = () => {
+  return (
+    <div className="placeholder">
+      <p className="place-title">Anagrams</p>
+      <p className="phonetic">/ˈanəˌɡram/</p>
+      <p className="noun">noun</p>
+      <p className="meaning">“a word or phrase made by using the letters of another word or phrase in a different order”</p>
+      <p className="example">“Angel” is an anagram of “glean.”</p>
+    </div>
+  );
+};
+export default PlaceHolder;
